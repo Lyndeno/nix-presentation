9f72036f109d98aee946d884de32ad2301ddfe1d
Lyndon Sanche <lsanche@lyndeno.ca>
2024-05-18 13:07:56 -0600

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Lyndon Sanche's resume";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+  }:
+    with flake-utils.lib;
+      eachSystem defaultSystems (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        tex = pkgs.texlive.withPackages (ps:
+          with ps; [
+            scheme-basic
+            latex-bin
+            latexmk
+            titlesec
+            fontawesome
+            fontspec
+            xcolor
+            dashrule
+            ifmtarg
+            enumitem
+            hyphenat
+            lato
+            fontaxes
+            xkeyval
+          ]);
+      in rec {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              alejandra.enable = true;
+            };
+          };
+        };
+        packages = {
+          resume = pkgs.stdenvNoCC.mkDerivation rec {
+            name = "nix-presentation";
+            src = self;
+            buildInputs = [pkgs.gnumake pkgs.coreutils tex];
+            phases = ["unpackPhase" "buildPhase" "installPhase"];
+            installPhase = ''
+              mkdir -p $out
+              cp nix-presentation.pdf $out/
+            '';
+          };
+          default = packages.resume;
+        };
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = [packages.default.buildInputs];
+          };
+        };
+      });
+}
